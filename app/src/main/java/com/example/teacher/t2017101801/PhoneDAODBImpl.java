21888e9e821bd6d01e9e206fb678bf52f4eca01b
Shaojung Lu <shaojung@gmail.com>
2017-10-18 13:34:54 +0800

    Start DAO

--- app/src/main/java/com/example/teacher/t2017101801/PhoneDAODBImpl.java
+++ app/src/main/java/com/example/teacher/t2017101801/PhoneDAODBImpl.java
@@ -1,11 +1,20 @@
 package com.example.teacher.t2017101801;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by teacher on 2017/10/18.
  */
 
 public class PhoneDAODBImpl implements PhoneDAO {
+    SQLiteDatabase db;
 
+    public PhoneDAODBImpl(Context context)
+    {
+        MyHelper helper = new MyHelper(context);
+        db = helper.getWritableDatabase();
+    }
 
     @Override
     public void addOne(Phone p) {
