391a0528376f34ad931536d70c874fbfe653d64b
Shaojung Lu <shaojung@gmail.com>
2017-10-18 13:19:19 +0800

    Start DAO

---
+++ app/src/main/java/com/example/teacher/t2017101801/PhoneDAO.java
@@ -0,0 +1,10 @@
+package com.example.teacher.t2017101801;
+
+/**
+ * Created by teacher on 2017/10/18.
+ */
+
+public interface PhoneDAO {
+    public void addOne(Phone p);
+    public Phone getOne(int id);
+}
---
+++ app/src/main/java/com/example/teacher/t2017101801/PhoneDAODBImpl.java
@@ -0,0 +1,19 @@
+package com.example.teacher.t2017101801;
+
+/**
+ * Created by teacher on 2017/10/18.
+ */
+
+public class PhoneDAODBImpl implements PhoneDAO {
+
+
+    @Override
+    public void addOne(Phone p) {
+
+    }
+
+    @Override
+    public Phone getOne(int id) {
+        return null;
+    }
+}
