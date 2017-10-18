88d8fa292e43333b7b569be7507e9d5d3022ac32
Shaojung Lu <shaojung@gmail.com>
2017-10-18 11:34:34 +0800

    use helper

--- app/src/main/java/com/example/teacher/t2017101801/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017101801/MainActivity.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,21 +16,43 @@ public class MainActivity extends AppCompatActivity {
     }
     public void click1(View v)
     {
+        Phone p = new Phone();
+        p.name = "BBB";
+        p.tel = "123";
+        p.addr = "aabb";
+        add(p);
+
+        Phone p1 = getOne(1);
+
+
+    }
+
+    public void add(Phone p)
+    {
         MyHelper helper = new MyHelper(MainActivity.this);
         SQLiteDatabase db = helper.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put("name", "AAA");
-        cv.put("tel", "1111");
-        cv.put("addr", "aabb123");
+        cv.put("name", p.name);
+        cv.put("tel", p.tel);
+        cv.put("addr", p.addr);
 
         db.insert("phone", null, cv);
-        
-        Cursor c = db.query("phone", new String[] {"name"}, null, null, null, null, null);
-        c.moveToFirst();
-        Log.d("DB", c.getString(0));
-
         db.close();
+    }
 
+    public Phone getOne(int id)
+    {
+        Phone p = new Phone();
+        MyHelper helper = new MyHelper(MainActivity.this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor c = db.query("phone", new String[] {"id","name","tel","addr"}, "id=?", new String[] {String.valueOf(id)}, null, null, null);
+        c.moveToFirst();
+        p.id = c.getInt(0);
+        p.name = c.getString(1);
+        p.tel = c.getString(2);
+        p.addr = c.getString(3);
+        db.close();
+        return p;
     }
 
 }
