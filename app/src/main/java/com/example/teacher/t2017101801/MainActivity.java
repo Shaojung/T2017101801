e61ac128ca90a975a01f79d081ec4cb9803585d2
Shaojung Lu <shaojung@gmail.com>
2017-10-18 10:53:58 +0800

    use helper

--- app/src/main/java/com/example/teacher/t2017101801/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017101801/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.teacher.t2017101801;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
     {
         MyHelper helper = new MyHelper(MainActivity.this);
         SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name", "AAA");
+        cv.put("tel", "1111");
+        cv.put("addr", "aabb123");
+
+        db.insert("phone", null, cv);
+
+
+        Cursor c = db.query("phone", new String[] {"name"}, null, null, null, null, null);
+        c.moveToFirst();
+        Log.d("DB", c.getString(0));
+
+        db.close();
 
     }
 
