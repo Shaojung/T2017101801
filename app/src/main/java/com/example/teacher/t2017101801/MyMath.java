0b0e6787e11117995a51a64ad1f130f1afc79308
Shaojung Lu <shaojung@gmail.com>
2017-10-18 14:10:56 +0800

    UnitTest

---
+++ app/src/main/java/com/example/teacher/t2017101801/MyMath.java
@@ -0,0 +1,11 @@
+package com.example.teacher.t2017101801;
+
+/**
+ * Created by teacher on 2017/10/18.
+ */
+
+public class MyMath {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
---
+++ app/src/test/java/com/example/teacher/t2017101801/MyTest1.java
@@ -0,0 +1,18 @@
+package com.example.teacher.t2017101801;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by teacher on 2017/10/18.
+ */
+
+public class MyTest1 {
+    @Test
+    public void myTest()
+    {
+        assertEquals(10, MyMath.add(3,7));
+    }
+}
