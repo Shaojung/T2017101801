ddbada06bc76005197dcb9943650c8f8eac0b11d
Shaojung Lu <shaojung@gmail.com>
2017-10-18 11:36:35 +0800

    use helper

---
+++ app/src/main/java/com/example/teacher/t2017101801/Phone.java
@@ -0,0 +1,12 @@
+package com.example.teacher.t2017101801;
+
+/**
+ * Created by teacher on 2017/10/18.
+ */
+
+public class Phone {
+    public int id;
+    public String name;
+    public String tel;
+    public String addr;
+}
