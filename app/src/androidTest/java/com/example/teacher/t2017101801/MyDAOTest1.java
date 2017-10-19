d4afe9e5aef0ef787ec3500fc68d5a3864bdc98d
Shaojung Lu <shaojung@gmail.com>
2017-10-19 10:32:42 +0800

    DAO

--- app/src/androidTest/java/com/example/teacher/t2017101801/MyDAOTest1.java
+++ app/src/androidTest/java/com/example/teacher/t2017101801/MyDAOTest1.java
@@ -31,4 +31,38 @@ public class MyDAOTest1 {
         assertEquals("BBB", pArray[0].name);
 
     }
+
+    @Test
+    public void testDelete()
+    {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        PhoneDAODBImpl dao = new PhoneDAODBImpl(appContext);
+        Phone p1 = new Phone("CCC", "333", "CC33");
+        Phone p2 = new Phone("DDD", "444", "DD44");
+
+        dao.clearAll();
+        dao.addOne(p1);
+        dao.addOne(p2);
+        Phone pArray[] = dao.getList();
+        p1.id = pArray[0].id;
+        dao.delete(p1);
+        Phone pArray2[] = dao.getList();
+        assertEquals("DDD", pArray2[0].name);
+    }
+    @Test
+    public void testUpdate()
+    {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        PhoneDAODBImpl dao = new PhoneDAODBImpl(appContext);
+        Phone p1 = new Phone("CCC", "333", "CC33");
+
+        dao.clearAll();
+        dao.addOne(p1);
+        Phone pArray[] = dao.getList();
+        p1.id = pArray[0].id;
+        p1.name = "CDE";
+        dao.update(p1);
+        Phone pArray2[] = dao.getList();
+        assertEquals("CDE", pArray2[0].name);
+    }
 }
